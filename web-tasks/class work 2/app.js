d12845b86e7acbbd7040207667bc6b70c7ea1be6
itzCherryYT <100517452+GeflixAdministrator@users.noreply.github.com>
2023-06-29 01:46:32 +0400

    Add files via upload

---
+++ web-tasks/class work 2/app.js
@@ -0,0 +1,11 @@
+const linearSearch = (arr, item) => {
+    for (const i in arr) 
+    {
+      if (arr[i] === item) 
+        return +i;
+    }
+
+  };
+  const nums = [15, 12, 3, 6, 34, 64, 77, 118, 59]
+
+  
