b2a52bccfd0f1cc128073d91f15e988cb2072ddb
itzCherryYT <100517452+GeflixAdministrator@users.noreply.github.com>
2023-06-29 01:45:15 +0400

    Add files via upload

---
+++ task 5/app.js
@@ -0,0 +1,27 @@
+let array = [1,54,24,86,35,7,82,5,];
+
+let min = array[ 0 ];
+let max = 0;
+let len = array.length;
+
+for ( let i = 0; i < len - 1; i++) {
+    if ( min > array[ i ]) {
+        min = array[ i ];
+    }
+
+    if ( max < array[ i ]) { 
+        max = array[ i ];
+    }
+
+    min;
+    max;
+}
+
+console.log("უმცირესი არის : " + min );
+console.log("უდიდესი არის : " + max);
+
+let რიცხვები = [1,54,24,86,35,7,82,5];
+
+რიცხვები.sort((x, y) => x - y);
+
+    console.log("დალაგდა ზრდის მიხედვით : ", რიცხვები)
